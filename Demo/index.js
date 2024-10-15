0f8890554ec6e619db3d8c0a4f0ae88e2c5276c8
Renzo Kooi <renzo.kooi@gmail.com>
2024-10-15 13:48:58 +0200

    wrap demo page

--- Demo/index.js
+++ Demo/index.js
@@ -18,31 +18,39 @@ function demo() {
     text: "Back to repository"
   });
   
-  const detailsContent = DIV(
-    DIV("This library offers an alternative for the ", link2Repo, " library."),
-    DIV("Basically the same idea, but a bit more versatile. Address className as ",
-      CODE("{class: ...}"), ", data-attributes as ",
-      CODE("{data: {one: '...', ...} }"), " and innerHTML/textContent as",
-      CODE("{text: ...}"), " or ", CODE("{html: ...}"), "."),
-    DIV("Furthermore, the tags can be used <i>case insensitive</i> (e.g. ",
-      CODE("const {h3, H3, DIV, p, P} = $T"),
-      "), and some attributes/properties considered <i>evil</i> are prohibited. "),
-    DIV("The library uses a ", CODE("Proxy"),
-      ", so the elements are lazy loaded (on demand)."),
-    DIV({text: "Enjoy!"})
+  const detailsContent =
+    DIV(
+      DIV("This library offers an alternative for the ", link2Repo, " library."),
+      DIV("Basically the same idea, but a bit more versatile. Address className as ",
+        CODE("{class: ...}"), ", data-attributes as ",
+        CODE("{data: {one: '...', ...} }"), " and innerHTML/textContent as",
+        CODE("{text: ...}"), " or ", CODE("{html: ...}"), "."),
+      DIV("Furthermore, the tags can be used <i>case insensitive</i> (e.g. ",
+        CODE("const {h3, H3, DIV, p, P} = $T"),
+        "), and some attributes/properties considered <i>evil</i> are prohibited. "),
+      DIV("The library uses a ", CODE("Proxy"),
+        ", so the elements are lazy loaded (on demand)."),
+      DIV({text: "Enjoy!"})
   );
   
-  $("body").append(
-    topLink,
-    DETAILS({open: true}, SUMMARY("<span>About</span>"), detailsContent),
-    DIV({id: "NameDiv"}, H3({data: {name: "Mary POC Demo"}}), $T.p("How are <i><b>you</b></i> today?")),
+  document.body.append(
+    DIV({class: "container"},
+      DIV({class: "content"},
+        topLink,
+        DETAILS({open: true}, SUMMARY("<span>About</span>"), detailsContent),
+        DIV({id: "NameDiv"},
+          H3({data: {name: "Mary POC Demo"}}),
+          $T.p("How are <i><b>you</b></i> today?")
+        )
+      )
+    )
   );
 }
 
 // code
 function createCodeDetails() {
   const demoCode = demo.toString();
-  document.body.append(
+  $(`.content`).append(
     $T.details(
       $T.summary(`<span>The code for the above</span>`),
       $T.pre({class: `language-javascript line-numbers`},
