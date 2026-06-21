b7bc1195f4778f94f6b9077dda3e5108060ca758
Renzo Kooi <renzo.kooi@gmail.com>
2026-06-21 16:12:46 +0200

    Demo redo

--- Demo/index.js
+++ Demo/index.js
@@ -1,19 +1,24 @@
+import {default as CreateComponent, createOrRetrieveShadowRoot} from "./es-webcomponent-bundle.js";
 import $T from "../tinyDOM.js";
 addHandler();
 demo();
 createCodeDetails();
+window.$T = $T;
+createCopyrightComponent();
 
 function demo() {
   // imported with
   // import $T from "../tinyDOM.js";
   // ------------------------------------------------
-  const {H3, DIV, A, CODE, DETAILS, SUMMARY, P, p, I, B, NOTHING} = $T;
+  $T.setError = key => $T.b(
+    {style: `color: red`},
+    $T.code(key), ` is `,
+    $T.i(`not`), ` a valid HTML tag`);
+  
+  // destructure relevant tag functions
+  const { H3, DIV, A, CODE, DETAILS, SUMMARY, P, p, I, B, NOTHING, } = $T;
   const revealCodeLink = text => I({data: {action: "revealCode"}, text});
   const printHTML = elem => elem.outerHTML.replace(/</g, "&lt;");
-  const back2RepoLink = A( {
-    target: "_top",
-    href: "https://github.com/KooiInc/tinyDOM",
-    text: "Back to repository" } );
   const jqlLink = A({
     target: "_blank",
     class: "linkJQL",
@@ -24,38 +29,48 @@ function demo() {
     DIV(
       DIV("(", revealCodeLink("Click here"),
         " to reveal the code used to create the html in this demonstration page)"),
-      DIV("This small <i>library</i> offers a way to dynamically create HTML elements\
+      DIV("This small <i>library</i> offers a way to dynamically create (nested) HTML elements\
          by converting tag names (<code>div</code> <code>table</code> etc.)\
          to element creation functions ('", I("tag functions"), "')."),
-      DIV("Some examples (the default library export was imported as ", CODE("tags"), ")"),
+      DIV("Some examples (the default library export was imported as ", CODE("$T"), ")"),
       $T.UL(
-        $T.li($T.code("tags.div('Hello world')"),
+        $T.li($T.code("$T.div('Hello world')"),
           " => ", printHTML($T.div("Hello world"))),
-        $T.li($T.code("tags.p({class: 'helloworld', data: {world: ' World'}}, 'Hello')"),
+        $T.li($T.code("$T.p({class: 'helloworld', data: {world: ' World'}}, 'Hello')"),
           " =>", $T.br(),
           printHTML($T.p({class: 'helloworld', data: {world: ' World'}}, 'Hello'))),
-        $T.li($T.code("tags.span({text: 'Hello world'}"),
+        $T.li($T.code("$T.span({text: 'Hello world'})"),
           " => ", printHTML($T.span({text: 'Hello world'}))),
-        $T.li("<code>tags.P('hello &lt;i>world&lt;/i>')</code>",
+        $T.li("<code>$T.P('hello &lt;i>world&lt;/i>')</code>",
           " => ", printHTML($T.P("hello <i>world</i>"))),
-        $T.li($T.CODE("const {p, P} = tags; p('hello world')"),
+        $T.li($T.div({class: `noMargin`}, "Destructured:"),
+          $T.CODE("const {p, P} = $T; p('hello world')"),
           " => ", printHTML(p("hello world"))),
       ),
       DIV("A tag function is <i>case insensitive</i> (so ",
-        CODE("tags.DIV"), " / ", CODE("tags.div"), " / ", CODE("tags.dIV"), " are equal)."),
-      DIV("The properties for a HTML element, e.g. ",
+        CODE("$T.DIV"), " / ", CODE("$T.div"), " / ", CODE("$T.diV"), " are equal)."),
+      DIV("Properties for a HTML element, e.g. ",
         CODE("class"), " or ", CODE("id"), " can be given as an object in the\
           first argument. Everything from the next argument(s) is nested\
           within the created element. It may be strings or other HTML elements.\
-          Strings may contain html."),
-      DIV("Invalid tagnames will be converted to a function returning a ",
-        CODE("&lt;b style:'color:red'>"), " element containing an error message.\
-        See ", CODE("NOTHING()"), " in the ",
+          Strings can be plain text or HTML."),
+      DIV("Invalid tagnames will be converted to a function returning nothing by\
+        default. The 'error'-function may be re-assigned, e.g. to a function returning\
+        an element containing an error message, or a function reporting an error\
+        in the console. Use the setter ",
+        DIV({class: `codeCenter`}, "<code>$T.setError = function([key]) {return ...})</code>"),
+        "for that. See ", CODE("NOTHING()"), " in the ",
         revealCodeLink("example code")
       ),
+      $T.div("Autonomous custom elements can also be used. The top bar of this demonstration " +
+        "page is a small web component: <code>&lt;copyright-slotted&gt;</code>. It is created using ",
+        $T.code('$T["copyright-slotted"](...)'), ", which can also be ",
+        $T.code('$T.copyrightSlotted(...)'), `.`),
       DIV("The library uses a ", CODE("Proxy"),
-        ", so the tag functions are <i>lazy loaded</i> (on demand)."),
-      DIV("The library is included an used in ", jqlLink, $T.SPAN(` (a jQuery alike module)`) ),
+        ", so every tag function is <i>lazy loaded</i> (on demand)."),
+      DIV("The library is included an used in ", jqlLink, $T.SPAN(` (a jQuery alike module).`) ),
+      DIV("This document is completely created using the tinyDOM library (use CTRL+U to " +
+        "see the HTML source of the original document).") ,
       DIV({text: "Enjoy!"})
     );
   
@@ -65,17 +80,14 @@ function demo() {
     $T.comment(`hello, this is a comment`),
     DIV( {class: "container"},
       DIV( {class: "content"},
-        back2RepoLink,
         DETAILS({open: true}, SUMMARY("<span>About</span>"), aboutContent),
         DIV({id: "NameDiv"},
           H3({data: {name: "Mary POC Demo"}}),
           P("How are ", I(B("you")), " today?"),
-          DIV("Let's check this NOTHING tag function (",
-            CODE("NOTHING()"),
-            ", see ",
-            revealCodeLink("Code"),
-           ") => ",
-            NOTHING()
+          DIV("After rewriting the error function using <code>$T.setError</code>, ",
+            $T.br(), "let's check this NOTHING tag function (", CODE("NOTHING()"),
+            ", see ", revealCodeLink("Code"), ") => ",
+            $T.div(NOTHING())
           )
         )
       )
@@ -106,4 +118,34 @@ function createCodeDetails() {
     )
   );
   Prism.highlightAll();
+}
+
+// custom autonomous element (web component)
+// see https://github.com/KooiInc/es-webcomponent-factory
+function createCopyrightComponent() {
+  CreateComponent( { componentName: `copyright-slotted`, onConnect: copyrightComponentConnectHandler });
+  renderCopyrightComponent();
+}
+
+function renderCopyrightComponent() {
+  const {copyrightSlotted} = $T;
+  const ghLink = $T.a({
+      slot: `link`,
+      href: `//github.com/KooiInc/tinyDOM`,
+      target: `_top`,
+      text: ` Back to repository`});
+  document.body.insertAdjacentElement( `afterbegin`,
+    copyrightSlotted(
+      $T.span({slot: `year`, class: `yr`, text: String(new Date().getFullYear())}),
+      ghLink,
+    )
+  );
+}
+
+function copyrightComponentConnectHandler(elem) {
+  const shadow = createOrRetrieveShadowRoot(elem);
+  const componentStyle = $T.style({textContent: `@import url(./cright.css)`});
+  const content = $T.div({html: `&copy; <span><slot name="year"></slot></span> KooiInc
+    | <span class="backlink">↺</span> <slot name="link"/>`});
+  shadow.append(content, componentStyle);
 }
